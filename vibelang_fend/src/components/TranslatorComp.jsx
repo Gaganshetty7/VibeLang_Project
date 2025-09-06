0ae3f0a658825475a700043d0caba8334758afe4
Gaganshetty7 <gaganrshetty07@gmail.com>
2025-09-06 13:41:17 +0530

    Created dropdowns for languages in TranslatorComp, added languages title and code in the reqd format given by the translation model documentations, handled each othe the dropdowns states

--- vibelang_fend/src/components/TranslatorComp.jsx
+++ vibelang_fend/src/components/TranslatorComp.jsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 
 const TranslatorComp = () => {
 
-  //counting maximum word limit
+  // #region counting maximum word limit
+
   const [text, setText] = useState('');
   const maxWords = 110;
 
-  const handleChange = (e) => {
+  const handleChangeTextInput = (e) => {
     const input = e.target.value;
     const wordsArray = input.trim().split(/\s+/).filter(word => word.length > 0);
     const wordCount = wordsArray.length;
@@ -17,11 +18,102 @@ const TranslatorComp = () => {
     }
   };
 
+  // #endregion
+
+  // #region Translation Logic and setting translated text
+
   const [translatedText, setTranslatedText] = useState('');
 
   const translate = () => {
     setTranslatedText(text);
   };
+
+  // #endregion
+
+  // #region List of languages for dropdowns
+
+  const languages = [
+    { title: "Arabic", code: "ar_AR" },
+    { title: "Czech", code: "cs_CZ" },
+    { title: "German", code: "de_DE" },
+    { title: "English", code: "en_XX" },
+    { title: "Spanish", code: "es_XX" },
+    { title: "Estonian", code: "et_EE" },
+    { title: "Finnish", code: "fi_FI" },
+    { title: "French", code: "fr_XX" },
+    { title: "Gujarati", code: "gu_IN" },
+    { title: "Hindi", code: "hi_IN" },
+    { title: "Italian", code: "it_IT" },
+    { title: "Japanese", code: "ja_XX" },
+    { title: "Kazakh", code: "kk_KZ" },
+    { title: "Korean", code: "ko_KR" },
+    { title: "Lithuanian", code: "lt_LT" },
+    { title: "Latvian", code: "lv_LV" },
+    { title: "Burmese", code: "my_MM" },
+    { title: "Nepali", code: "ne_NP" },
+    { title: "Dutch", code: "nl_XX" },
+    { title: "Romanian", code: "ro_RO" },
+    { title: "Russian", code: "ru_RU" },
+    { title: "Sinhala", code: "si_LK" },
+    { title: "Turkish", code: "tr_TR" },
+    { title: "Vietnamese", code: "vi_VN" },
+    { title: "Chinese", code: "zh_CN" },
+    { title: "Afrikaans", code: "af_ZA" },
+    { title: "Azerbaijani", code: "az_AZ" },
+    { title: "Bengali", code: "bn_IN" },
+    { title: "Persian", code: "fa_IR" },
+    { title: "Hebrew", code: "he_IL" },
+    { title: "Croatian", code: "hr_HR" },
+    { title: "Indonesian", code: "id_ID" },
+    { title: "Georgian", code: "ka_GE" },
+    { title: "Khmer", code: "km_KH" },
+    { title: "Macedonian", code: "mk_MK" },
+    { title: "Malayalam", code: "ml_IN" },
+    { title: "Mongolian", code: "mn_MN" },
+    { title: "Marathi", code: "mr_IN" },
+    { title: "Polish", code: "pl_PL" },
+    { title: "Pashto", code: "ps_AF" },
+    { title: "Portuguese", code: "pt_XX" },
+    { title: "Swedish", code: "sv_SE" },
+    { title: "Swahili", code: "sw_KE" },
+    { title: "Tamil", code: "ta_IN" },
+    { title: "Telugu", code: "te_IN" },
+    { title: "Thai", code: "th_TH" },
+    { title: "Tagalog", code: "tl_XX" },
+    { title: "Ukrainian", code: "uk_UA" },
+    { title: "Urdu", code: "ur_PK" },
+    { title: "Xhosa", code: "xh_ZA" },
+    { title: "Galician", code: "gl_ES" },
+    { title: "Slovene", code: "sl_SI" },
+  ];
+
+  // #endregion
+
+  // #region states for selected source and selected target languages from dropdowns
+
+  // #region states for selected source language from dropdown
+
+  const [selectedSourceLang, setSelectedSourceLang] = useState('');
+
+  const handleSourceLangChange = (e) => {
+    setSelectedSourceLang(e.target.value);
+  }
+
+
+  // #endregion
+
+  // #region states for selected target language from dropdown
+
+  const [selectedTargetLang, setSelectedTargetLang] = useState('');
+
+  const handleTargetLangChange = (e) => {
+    setSelectedTargetLang(e.target.value);
+  }
+
+  // #endregion
+
+  // #endregion
+
 
 
   return (
@@ -31,7 +123,12 @@ const TranslatorComp = () => {
       <div style={styles.leftPanel}>
 
         <div style={styles.topBtns}>
-          <button style={styles.actionBtn}>Detect language</button>
+          <select style={styles.langDropDown} onChange={handleSourceLangChange} value={selectedSourceLang}>
+            <option>Choose Language</option>
+            {languages.map((lang) => (
+              <option key={lang.code} value={lang.code}>{lang.title}</option>
+            ))}
+          </select>
         </div>
         <div style={styles.leftPanelBody}>
           <div style={styles.enterText}>
@@ -41,7 +138,7 @@ const TranslatorComp = () => {
               cols="50"
               placeholder="Enter text here..."
               value={text}
-              onChange={handleChange} />
+              onChange={handleChangeTextInput} />
           </div>
           <div style={styles.wordCount}>
             {text ? text.trim().split(/\s+/).length : 0} / {maxWords}
@@ -53,8 +150,14 @@ const TranslatorComp = () => {
       <div style={styles.rightPanel}>
 
         <div style={styles.topBtns}>
-          <button style={styles.actionBtn}>Choose Language</button>
+          <select style={styles.langDropDown} onChange={handleTargetLangChange} value={selectedTargetLang}>
+            <option>Choose Language</option>
+            {languages.map((lang) => (
+              <option key={lang.code} value={lang.code}>{lang.title}</option>
+            ))}
+          </select>
         </div>
+
         <div style={styles.rightPanelBody}>
           <textarea
             value={translatedText}
@@ -97,7 +200,8 @@ const styles = {
     display: 'flex',
     gap: '12px',
     justifyContent: 'center',
-    marginBottom: '5px'
+    marginBottom: '5px',
+    overflow: 'none',
   },
   leftPanelBody: {
     flex: 1,
@@ -142,11 +246,11 @@ const styles = {
     boxSizing: 'border-box',
   },
 
-  actionBtn: {
+  langDropDown: {
     border: '2px solid #000',
     color: '#003973',
     background: '#fff',
-    padding: '8px 22px',
+    padding: '8px 20px',
     borderRadius: '30px',
     fontWeight: '300',
     fontSize: '18px',
